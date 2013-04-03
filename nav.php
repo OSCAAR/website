9a15ed1cf6f626b6a08a7212797afd38712936bd
SamTheGeek <sam@samthegeek.com>
2013-04-02 23:46:40 -0400

    Converted navbar to PHP
    Moved the navbar to the nav.php file. Replaced it in all pages with php
    code.

---
+++ nav.php
@@ -0,0 +1,31 @@
+<div id='navbar'>
+	<table style="border-style: outset; border-color: #999999;">
+		<tr>
+			<td>
+				<img align="left" src="images/sumoLarge2cropped.png" width="328" height="100" alt="SUMO"/>
+				<td width=100% height="100" align="center" />
+				<img align="right" src="images/University_of_Maryland.png" width="100" height="100" alt="SUMO"/>
+			</td>
+		</tr>
+	</table>
+	<table bgcolor=#900000 style="border-color: #CCCC00; width:100%; border-style: outset;">
+			<tr>
+				<td align=center >
+					<a href="Homepage.html">Home</a>
+				</td>
+				<td align=center >
+					<a href="ObsBios.html">Our Team </a>
+				</td>
+				<td align=center >
+					<a href="TelescopeBios.html">Equipment </a>
+				</td>
+				<td align=center >
+					<a href="OSCAARbio.html">OSCAAR </a>
+				</td>   
+	         <td align=center >
+	         	<a href="Asteroids.html">Asteroids </a>
+	         </td>
+	          
+		</tr>
+	</table>
+</div>
